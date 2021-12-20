aca97ad77d665a7813054636f92c064de23b5050
fdaniel713 <fuentesdaniel342@gmail.com>
2021-12-20 19:32:30 -0300

    lista de productos con sus precios

---
+++ src/components/HeaderApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Row, Col} from "react-bootstrap";
+
+
+export const HeaderApp =() =>(
+<Row>
+    <Col>
+    <div>Mini Ecommerce</div>   
+    </Col>
+</Row>
+
+
+);
