48fbeee89962b40b303f10c9888893bcc9ecbe75
fdaniel713 <fuentesdaniel342@gmail.com>
2021-12-28 19:12:17 -0300

    agregando carrito

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { HeaderApp } from "./components/HeaderApp";
-import { ListProducts } from "./components/ListProducts";
+import { ListProducts } from "./components/Productos/ListProducts";
 
 const App = () => {
   return (
--- src/components/HeaderApp.js
+++ src/components/HeaderApp.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
 import logo from '../logo.svg'
+import { ShoppingCart } from "./ShoppingCard/shoppingCard";
 
 const style = {
   marginBottom: '30px'
 }
+
 export const HeaderApp = () => (
-    <Navbar bg="dark" variant="dark" style={style}>
+  <Navbar bg="dark" variant="dark" style={style}>
     <Container>
       <Navbar.Brand href="#home">
         <img
@@ -18,7 +20,7 @@ export const HeaderApp = () => (
         />{' '}
       Mini Ecommerce
       </Navbar.Brand>
+      <ShoppingCart />
     </Container>
   </Navbar>
 );
-
